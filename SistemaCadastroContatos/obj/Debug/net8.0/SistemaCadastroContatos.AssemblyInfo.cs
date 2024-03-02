87518d0b84f10c8ab25d03403c4411ad7a4033ff
Keven <kevenchristiancode@gmail.com>
2024-03-02 15:32:31 -0300

    :construction: construindo tabela

--- SistemaCadastroContatos/obj/Debug/net8.0/SistemaCadastroContatos.AssemblyInfo.cs
+++ SistemaCadastroContatos/obj/Debug/net8.0/SistemaCadastroContatos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaCadastroContatos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9630060e5872aae50a77b73571ba778c73964e10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7aebbced4ccad6755a4c9e3df73ac24d4871549c")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaCadastroContatos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaCadastroContatos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
